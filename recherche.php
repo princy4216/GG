1828cc46e44a0ee0d1c7010f4c2c0afa76717bec
mihantsa19 <mihantsarajaonarivelo@gmail.com>
2025-06-30 10:50:40 +0300

    recherche

--- recherche.php
+++ recherche.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && (
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Recherche Employés</title>
+    <title>Recherche. Employés</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="p-5">
