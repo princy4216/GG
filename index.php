575d9f23b1cee566f9b89b631cb9fcd02cb9e8b0
mihantsa19 <mihantsarajaonarivelo@gmail.com>
2025-06-30 11:01:17 +0300

    index

--- index.php
+++ index.php
@@ -7,45 +7,51 @@ $departments = get_departments_with_managers();
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Liste des départements</title>
+    <title>Liste des Départements</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
 </head>
-<body class="p-5">
+<body class="bg-light p-4">
 
-<h1>Liste des départements et managers</h1>
+<div class="container">
+    <div class="d-flex justify-content-between align-items-center mb-4">
+        <h1 class="text-primary">Départements & Managers</h1>
+        <a href="recherche.php" class="btn btn-success">🔍 Rechercher un employé</a>
+    </div>
 
-<table class="table table-bordered">
-    <thead>
-        <tr>
-            <th>Département</th>
-            <th>Manager</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($departments as $dept) { ?>
-            <tr>
-                <td>
-                    <a href="departement.php?dept_no=<?php echo $dept['dept_no']; ?>">
-                        <?php echo $dept['dept_name']; ?>
-                    </a>
-                </td>
-                <td>
-                    <?php
-                    if ($dept['first_name']) {
-                        echo $dept['first_name'] . ' ' . $dept['last_name'];
-                    } else {
-                        echo 'Aucun manager';
-                    }
-                    ?>
-                </td>
-            </tr>
-        <?php } ?>
-    </tbody>
-</table>
-<a href="recherche.php" class="btn btn-success">🔍 Rechercher un employé</a>
+    <div class="card shadow-sm">
+        <div class="card-body">
+            <table class="table table-hover align-middle">
+                <thead class="table-primary">
+                    <tr>
+                        <th>Département</th>
+                        <th>Manager</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($departments as $dept) { ?>
+                        <tr>
+                            <td>
+                                <a href="departement.php?dept_no=<?php echo $dept['dept_no']; ?>" class="text-decoration-none">
+                                    <?php echo $dept['dept_name']; ?>
+                                </a>
+                            </td>
+                            <td>
+                                <?php
+                                if ($dept['first_name']) {
+                                    echo $dept['first_name'] . ' ' . $dept['last_name'];
+                                } else {
+                                    echo '<span class="text-muted">Aucun manager</span>';
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
 </body>
-
-
-
 </html>
